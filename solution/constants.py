706ea1b7722f7de6ee008cefbcd17b261787f604
Ben Graham <bgraham@bgraham.com.au>
2012-05-02 09:58:31 +1000

    best solutions are now partially combined as well as mutated

--- solution/constants.py
+++ solution/constants.py
@@ -1,7 +1,7 @@
 # amount to vary terms by if mutating. 0.1 = +/- 10%.
-MUTE_VARIABILITY = 0.3
+MUTE_VARIABILITY = 0.2
 
-NUM_MUTE_TERMS = 10
+NUM_MUTE_TERMS = 5
 
 # chance to add new terms to a term set
 CREATE_TERM_CHANCE = 0.2
@@ -15,9 +15,9 @@ BRANCHING_FACTOR = 3
 # how much to scale down multipliers if an overflow occurs
 OVERFLOW_REDUCTION_DIVISOR = 2
 
-PLOT_SIZE = 256
+PLOT_SIZE = 512
 
 ORIGIN_X = PLOT_SIZE / 2
 ORIGIN_Y = PLOT_SIZE / 2
 
-RECURSION_LIMIT = 6
+RECURSION_LIMIT = 7
--- solution/models.py
+++ solution/models.py
@@ -1,83 +1,84 @@
-from django.db import models
-from math import sin, cos, e, hypot
-from solution import constants
+from math import sin, cos, e, pi, hypot, copysign, log
+import sys
+import constants
 from random import choice, random, seed
 from PIL import Image, ImageDraw
+from multiprocessing import Pool
 
 
-class TermSet(models.Model):
+class TermSet(object):
+	def __init__(self, terms=[]):
+		self.terms = terms
+		if not self.terms:
+			self._add_term()
+
 	def __unicode__(self):
 		from string import join
-		terms = self.baseterm_set.all()
-		return join(map(lambda s: str(s), terms), ' + ')
+		return join(map(lambda s: str(s), self.terms), ' + ')
 
 	def f(self, x):
-		terms = self.baseterm_set.all()
-		s = sum(map(lambda f: f.f(x), terms))
-		return s
+		return sum(map(lambda f: f.f(x), self.terms))
+
+	def _add_term(self):
+		seed()
+		term_type = choice(BaseTerm.TERM_TYPES)
+		bt = BaseTerm(term_type=term_type)
+		self.terms.append(bt)
+
+	def _delete_term(self):
+		t = choice(self.terms)
+		self.terms.remove(t)
 
 	def mutate(self):
 		mutate_count = 0
-		terms = self.baseterm_set.all()
 
 		# maybe delete a term
-		if terms and random() > (1 - constants.DELETE_TERM_CHANCE):
-			choice(self.baseterm_set.all()).delete()
+		if self.terms and random() > (1 - constants.DELETE_TERM_CHANCE):
+			self._delete_term()
 
 		# potentially create a new term. always do so if there are none.
-		if not terms or random() > (1 - constants.CREATE_TERM_CHANCE):
-			seed()
-			ttype = choice(('EXP', 'POLY', 'TRIG', 'CNST'))
-			if not terms:
-				# first term always a constant
-				ttype = 'CNST'
-			bt = BaseTerm(term_set_id=self.pk, term_type=ttype)
-			bt.mutate()
-			bt.save()
+		if not self.terms or random() > (1 - constants.CREATE_TERM_CHANCE):
+			self._add_term()
 
 		# modify some term constants
-		if terms:
+		if self.terms:
 			while mutate_count < constants.NUM_MUTE_TERMS:
-				t = choice(terms)
-				t.mutate()
-				t.save()
+				if random() > 0.5:
+					t = choice(self.terms)
+					t.mutate()
 				mutate_count += 1
 
 	def copy(self):
-		orig_terms = self.baseterm_set.all()
-		ts = TermSet()
-		ts.save()
-		for orig_term in orig_terms:
-			new_term = orig_term.copy(term_set_id=ts.id)
-			new_term.save()
+		ts = TermSet(self.terms[:])
 		return ts
 
 
-class BaseTerm(models.Model):
+class BaseTerm(object):
 	TERM_TYPES = (
-		(u'EXP',  u'Exponential'),
-		(u'POLY', u'Polynomial'),
-		(u'TRIG', u'Trigonometric'),
-		(u'CNST', u'Constant'),
+		(u'EXP'),
+		(u'POLY'),
+		(u'TRIG'),
+		(u'CNST'),
 	)
-	outerMultiplier = models.FloatField(default=1.0)
-	innerMultiplier = models.FloatField(default=1.0)
-	term_type = models.CharField(max_length=8, choices=TERM_TYPES)
-	term_set = models.ForeignKey(TermSet)
+
+	def __init__(self, term_type, innerMultiplier=1, outerMultiplier=1):
+		self.term_type = term_type
+		self.innerMultiplier = innerMultiplier
+		self.outerMultiplier = outerMultiplier
 
 	def __unicode__(self):
 		if self.term_type == 'EXP':
-			f = '{:.2}.e^({:.2}x)'
+			f = '{outer:.2}.e^({inner:.2}x)'
 		elif self.term_type == 'TRIG':
-			f = '{:.2}.sin({:.2}x)'
+			f = '{outer:.2}.sin({inner:.2}x)'
 		elif self.term_type == 'POLY':
-			f = '{:.2}.x^{:.2}'
+			f = '{outer:.2}.x^{inner:.2}'
 		elif self.term_type == 'CNST':
-			f = '({:.2}+{:.2})'
+			f = '({outer:.2}+{inner:.2})'
 		else:
-			return super(BaseTerm, self).__unicode__()
+			return "er..."
 
-		return f.format(self.outerMultiplier, self.innerMultiplier)
+		return f.format(outer=self.outerMultiplier, inner=self.innerMultiplier)
 
 	def f(self, x):
 		fx = None
@@ -88,7 +89,7 @@ class BaseTerm(models.Model):
 		elif self.term_type == 'POLY':
 			f = lambda i, o, x: o * e ** (x * i)
 		elif self.term_type == 'CNST':
-			f = lambda i, o, x: o + i
+			f = lambda i, o, x: o * i
 		else:
 			raise Exception("Term type unknown")
 
@@ -96,16 +97,15 @@ class BaseTerm(models.Model):
 			try:
 				fx = f(self.innerMultiplier, self.outerMultiplier, x)
 			except OverflowError:
-				print "Overflowed: " + self.__unicode__()
-				# do some fuzzing and reduction to try and avoid an overflow
-				self.innerMultiplier = (self.innerMultiplier + random()) / constants.OVERFLOW_REDUCTION_DIVISOR
-				self.outerMultiplier = (self.outerMultiplier + random()) / constants.OVERFLOW_REDUCTION_DIVISOR
+				# return largest float with correct sign
+				i_sign = copysign(1, self.innerMultiplier)
+				o_sign = copysign(1, self.innerMultiplier)
+				fx = copysign(sys.float_info.max, f(i_sign, o_sign, x))
 				pass
-			except Exception:
+			except ZeroDivisionError:
 				fx = 0
 				pass
 
-		# print self.__unicode__() + " = " + str(fx) + ", x = " + str(x)
 		return fx
 
 	def mutate(self):
@@ -120,28 +120,22 @@ class BaseTerm(models.Model):
 		self.outerMultiplier = scale(self.outerMultiplier)
 		return self
 
-	def copy(self, term_set_id):
-		c = BaseTerm()
-		c.innerMultiplier = self.innerMultiplier
-		c.outerMultiplier = self.outerMultiplier
-		c.term_type = self.term_type
-		c.term_set_id = term_set_id
-		return c
+	def copy(self):
+		bt = BaseTerm(
+			term_type=self.term_type,
+			innerMultiplier=self.innerMultiplier,
+			outerMultiplier=self.outerMultiplier
+		)
+		return bt
 
 
-class Solution(models.Model):
-	# parent = models.OneToOneField('self', null=True)
-	length_function = models.OneToOneField(TermSet, related_name='solution_length_function')
-	radiance_function = models.OneToOneField(TermSet, related_name='solution_radiance_function')
-	orientation_function = models.OneToOneField(TermSet, related_name='solution_orientation_function')
-	termination_function = models.OneToOneField(TermSet, related_name='solution_termination_function')
-
+class Solution(object):
 	def prime(self):
 		def create_termset():
 			ts = TermSet()
-			ts.save()
 			ts.mutate()
 			return ts
+		self.fitness = 0
 		self.length_function = create_termset()
 		self.radiance_function = create_termset()
 		self.orientation_function = create_termset()
@@ -174,19 +168,26 @@ class Solution(models.Model):
 		recurse(origin)
 		return points
 
-	def fitness(self):
+	def solve(self):
 		def minimum_service_distance(point):
-			return min([hypot(point[0] - s.x, point[1] - s.y) for s in point_set], key=abs)
+			try:
+				return min([hypot(point[0] - s.x, point[1] - s.y) for s in point_set], key=abs)
+			except OverflowError:
+				return sys.float_info.max
 
 		def point_length(point):
-			return sum([segment.length(self) for segment in point.segments(self)])
+			try:
+				return sum([segment.length(self) for segment in point.segments(self)])
+			except OverflowError:
+				return sys.float_info.max
 
-		eval_set = [(x, y) for x in range(0, constants.PLOT_SIZE, 16) for y in range(0, constants.PLOT_SIZE, 16)]
+		eval_set = [(x, y) for x in range(0, constants.PLOT_SIZE, 32) for y in range(0, constants.PLOT_SIZE, 32)]
 		point_set = self.point_set()
 		service_penalty = sum(map(minimum_service_distance, eval_set))
-		length_penalty = (sum(map(point_length, point_set)) / 100) ** 2
+		length_penalty = (sum(map(point_length, point_set)) / 10)
 		# print "  service: {}, length: {}".format(service_penalty, length_penalty)
-		return 1 / (service_penalty + length_penalty)
+		self.fitness = 1 / (service_penalty + length_penalty)
+		return self
 		
 
 class Point(object):
@@ -200,15 +201,21 @@ class Point(object):
 		self.dist_to_origin = self._dist_to_origin()
 
 	def _dist_to_origin(self):
-		return min(constants.PLOT_SIZE, hypot(self.x - constants.ORIGIN_X, self.y - constants.ORIGIN_Y), key=abs)
+		try:
+			return min(constants.PLOT_SIZE, hypot(self.x - constants.ORIGIN_X, self.y - constants.ORIGIN_Y), key=abs)
+		except OverflowError:
+			return constants.PLOT_SIZE
+			pass
+		except e:
+			raise e
 
 	def radiance(self, solution):
 		dist = self.dist_to_origin
-		return solution.radiance_function.f(dist)
+		return solution.radiance_function.f(dist) % (2 * pi)
 
 	def orientation(self, solution):
 		dist = self.dist_to_origin
-		return solution.orientation_function.f(dist)
+		return solution.orientation_function.f(dist) % (2 * pi)
 
 	def segments(self, solution):
 		orientation = self.orientation(solution)
@@ -237,10 +244,11 @@ class Segment(object):
 
 	def length(self, solution):
 		dist = self.base.dist_to_origin
-		return max(0, solution.length_function.f(dist))
+		length = max(0, solution.length_function.f(dist))
+		return log(length + 1) * 10
 
 	def end(self, solution):
-		#bound = lambda x: min(max(0, x), constants.PLOT_SIZE)
+		# bound = lambda x: min(max(0, x), constants.PLOT_SIZE)
 		x = self.base.x + self.length(solution) * cos(self.angle)
 		y = self.base.y + self.length(solution) * sin(self.angle)
 		return Point(x, y, self.base.depth + 1)
@@ -254,15 +262,62 @@ class Plot(object):
 		self.solution = solution
 
 	def draw(self, seq):
-		print "Drawing..."
 		im = Image.new('RGBA', (constants.PLOT_SIZE, constants.PLOT_SIZE), (30, 30, 30, 255))
 		draw = ImageDraw.Draw(im)
 		points = self.solution.point_set()
 		points.reverse()
 		for point in points:
-			c = 255 / (point.depth / 2 + 1)
+			c = 255 - (point.depth * 32)
 			for segment in point.segments(self.solution):
 				end = segment.end(self.solution)
 				draw.line((point.x, point.y, end.x, end.y), fill="rgb({r},{g},{b})".format(r=c, g=c, b=c))
 
 		im.save("/home/bgraham/dev_py/angion/out." + str(seq) + ".png", "PNG")
+
+
+def solve(solution):
+	return solution.solve()
+
+
+def new_solution():
+	s = Solution()
+	s.prime()
+	return s
+
+candidates = []
+solutions = []
+fittest = new_solution()
+solutions.append(fittest)
+last_fit = 0
+this_fit = 0
+pool = Pool(8)
+
+
+def cross(s1, s2):
+	sn = Solution()
+	sn.length_function = s1.length_function if random() > 0.5 else s2.length_function
+	sn.radiance_function = s1.radiance_function if random() > 0.5 else s2.radiance_function
+	sn.orientation_function = s1.orientation_function if random() > 0.5 else s2.orientation_function
+	sn.termination_function = s1.termination_function if random() > 0.5 else s2.termination_function
+	return sn
+
+for lap in range(100):
+	candidates = sorted(solutions, key=lambda s: s.fitness)[:2]
+	solutions.extend(candidates)  # keep the best few from previous round
+	solutions.extend([cross(s1, s2) for s1 in candidates for s2 in candidates])  # mix up the best few from previous round
+	solutions.extend([new_solution() for n in range(2)])  # add some fresh solutions
+
+	solutions = map(lambda s: s.mutate(), solutions)  # stir
+	solutions = pool.map(solve, solutions)  # compute point-sets and fitnesses
+
+	solutions.append(fittest)  # keep the best from last round, un-mutated
+	fittest = max(solutions, key=lambda s: s.fitness)
+
+	last_fit = this_fit
+	this_fit = fittest.fitness
+	improvement = 0 if last_fit == 0 else (this_fit / last_fit) - 1
+
+	print "fitness = {fitness} ({improvement:+.2%})".format(lap=lap, fitness=this_fit, improvement=improvement)
+
+	p = Plot(fittest)
+	p.draw(lap)
