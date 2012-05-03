0e0c8d0f75aa62cb3d1b225a14434d7dc882023d
Ben Graham <bgraham@bgraham.com.au>
2012-05-03 14:23:21 +1000

    image for README; misc code udpates

--- constants.py
+++ constants.py
@@ -1,16 +1,20 @@
 # amount to vary terms by if mutating. 0.1 = +/- 10%.
 MUTE_VARIABILITY = 0.2
 
-NUM_MUTE_TERMS = 4
+# scaled pos/neg adjustements aren't repeatable (e.g. 20 -10% + 10% =/= 20)
+# so we need a positive bias to adjust for it
+MUTE_POSITIVE_BIAS = 1 / ((1 - MUTE_VARIABILITY) ** 2)
+
+NUM_MUTE_TERMS = 2
 
 # maximum terms in a termset
 MAX_TERMS = 3
 
 # chance to add new terms to a term set
-CREATE_TERM_CHANCE = 0.15
+CREATE_TERM_CHANCE = 0.005
 
 # chance to delete a term (multiplied by number of terms)
-DELETE_TERM_CHANCE = 0.05
+DELETE_TERM_CHANCE = 0.0005
 
 # how many segments has each point?
 BRANCHING_FACTOR = 2
@@ -25,4 +29,4 @@ PLOT_MARGIN = 64
 ORIGIN_X = PLOT_SIZE / 2
 ORIGIN_Y = PLOT_SIZE / 2
 
-RECURSION_LIMIT = 9
+RECURSION_LIMIT = 6
--- fractal.py
+++ fractal.py
@@ -2,7 +2,7 @@ from math import sin, cos, e, pi, hypot, copysign
 import sys
 import constants
 import copy
-from random import choice, random, seed
+from random import choice, random, seed, uniform, triangular
 from PIL import Image, ImageDraw
 from multiprocessing import Pool
 seed()
@@ -84,7 +84,6 @@ class BaseTerm(object):
 		return f.format(outer=float(self.outerMultiplier), inner=float(self.innerMultiplier))
 
 	def f(self, x):
-		fx = None
 		if self.term_type == 'EXP':
 			f = lambda i, o, x: o * (x ** i)
 		elif self.term_type == 'TRIG':
@@ -96,51 +95,38 @@ class BaseTerm(object):
 		else:
 			raise Exception("Term type unknown")
 
-		while fx == None:
-			try:
-				fx = f(self.innerMultiplier, self.outerMultiplier, x)
-			except OverflowError:
-				# return largest float with correct sign
-				i_sign = copysign(1, self.innerMultiplier)
-				o_sign = copysign(1, self.innerMultiplier)
-				fx = copysign(sys.float_info.max, f(i_sign, o_sign, x))
-				pass
-			except ZeroDivisionError:
-				fx = 0
-				pass
+		# while fx == None:
+		try:
+			fx = f(self.innerMultiplier, self.outerMultiplier, x)
+		except OverflowError:
+			# return largest float with correct sign
+			i_sign = copysign(1, self.innerMultiplier)
+			o_sign = copysign(1, self.outerMultiplier)
+			fx = copysign(sys.float_info.max, f(i_sign, o_sign, x))
+			# pass
+		except ZeroDivisionError:
+			fx = 0
+			# pass
 
 		return fx
 
 	def mutate(self):
 		def scale(x):
-			if x == 0:
-				return random() - 0.5
-			vary = (random() - 0.5) * constants.MUTE_VARIABILITY * 2 * abs(x)
-			x = x + vary
-			if x > 100:
-				x = 100
-			elif x < -100:
-				x = -100
-			return x
-		self.innerMultiplier = scale(self.innerMultiplier)
-		self.outerMultiplier = scale(self.outerMultiplier)
+			return uniform(-1, 1)
+			return random() * constants.MUTE_VARIABILITY * abs(x) * choice([-1, constants.MUTE_POSITIVE_BIAS])
+			# if x > 100:
+			# 	x = 100
+			# elif x < -100:
+			# 	x = -100
+		self.innerMultiplier += scale(self.innerMultiplier)
+		self.outerMultiplier += scale(self.outerMultiplier)
 		return self
 
 
 class Solution(object):
-	def prime(self):
-		def create_termset():
-			seed()
-			ts = TermSet()
-			r = copy.deepcopy(ts)
-			r.mutate()
-			return r
 
+	def __init__(self):
 		self.fitness = 0
-		self.length_function = create_termset()
-		self.radiance_function = create_termset()
-		self.orientation_function = create_termset()
-		self.termination_function = create_termset()
 
 	def mutate(self):
 		self.length_function.mutate()
@@ -184,12 +170,11 @@ class Solution(object):
 		eval_set = [(x, y) for x in range(constants.PLOT_MARGIN, constants.PLOT_SIZE - constants.PLOT_MARGIN, 16) for y in range(constants.PLOT_MARGIN, constants.PLOT_SIZE - constants.PLOT_MARGIN, 16)]
 		point_set = self.point_set()
 
-		service_penalty = sum(map(minimum_service_distance, eval_set)) ** 0.5
-		length_penalty = sum(map(point_length, point_set)) ** 0.5
+		service_penalty = sum(map(minimum_service_distance, eval_set))
+		length_penalty = sum(map(point_length, point_set))
 		bounds_penalty = sum(map(bounds_penalty, point_set))
 
-		# print "  service: {}, length: {}".format(service_penalty, length_penalty)
-		self.fitness = 1 / (service_penalty + length_penalty + bounds_penalty)
+		self.fitness = 1 / (service_penalty + length_penalty + bounds_penalty + 1)
 		return self
 
 
@@ -210,22 +195,22 @@ class Point(object):
 			return min(constants.PLOT_SIZE, hypot(self.x - constants.ORIGIN_X, self.y - constants.ORIGIN_Y), key=abs)
 		except OverflowError:
 			return constants.PLOT_SIZE
-			pass
+			#pass
 		except e:
 			raise e
 
 	def radiance(self, solution):
 		dist = self.dist_to_origin
-		return solution.radiance_function.f(dist)  # % pi  # -90 --> +90
+		return solution.radiance_function.f(dist) % (2 * pi)
 
 	def orientation(self, solution):
 		dist = self.dist_to_origin
 		delta = solution.orientation_function.f(dist)  # % (pi * 2 / 8)) - ((pi * 2 / 8) / 2)  # -22.5 --> +22.5
-		return self.parent_orientation + delta
+		return (self.parent_orientation + delta) % (2 * pi)
 
 	def segments(self, solution):
-		orientation = self.orientation(solution) % (2 * pi)
-		radiance = self.radiance(solution) % (2 * pi)
+		orientation = self.orientation(solution)
+		radiance = self.radiance(solution)
 		sweep_begin = orientation - (radiance / 2)
 		sweep_step = radiance / (self.segment_count)
 		segments = []
@@ -267,24 +252,12 @@ class Segment(object):
 
 	def length(self, solution):
 		dist = self.base.dist_to_origin
-		#try:
 		return abs(solution.length_function.f(dist))
-		#except OverflowError:
-		#	return sys.float_info.max
-		#	pass
 
 	def end(self, solution):
-		# bound = lambda x: min(max(0, x), constants.PLOT_SIZE)
-		try:
-			x = self.base.x + self.length(solution) * cos(self.angle)
-			y = self.base.y + self.length(solution) * sin(self.angle)
-			return Point(x, y, self.base.depth + 1, self.angle)
-		except ValueError:
-			print "valueerror"
-			print self.length(solution)
-			print self.angle
-		except e:
-			raise e
+		x = self.base.x + self.length(solution) * cos(self.angle)
+		y = self.base.y + self.length(solution) * sin(self.angle)
+		return Point(x, y, self.base.depth + 1, self.angle)
 
 
 class Plot(object):
@@ -294,7 +267,13 @@ class Plot(object):
 		self.solution = solution
 
 	def draw(self, seq):
-		im = Image.new('RGBA', (constants.PLOT_SIZE, constants.PLOT_SIZE), (30, 30, 30, 255))
+		def colour_lookup(ratio):
+			r = 000 + (ratio * (42 - 000))
+			g = 150 + (ratio * (22 - 150))
+			b = 255 + (ratio * (69 - 255))
+			return "rgb({},{},{})".format(int(r), int(g), int(b))
+
+		im = Image.new('RGBA', (constants.PLOT_SIZE, constants.PLOT_SIZE), (10, 4, 27, 255))
 		draw = ImageDraw.Draw(im)
 		draw.line((
 			(constants.PLOT_MARGIN, constants.PLOT_MARGIN),
@@ -302,16 +281,17 @@ class Plot(object):
 			(constants.PLOT_SIZE - constants.PLOT_MARGIN, constants.PLOT_SIZE - constants.PLOT_MARGIN),
 			(constants.PLOT_SIZE - constants.PLOT_MARGIN, constants.PLOT_MARGIN),
 			(constants.PLOT_MARGIN, constants.PLOT_MARGIN)),
-			fill="rgb(0,0,0)"
+			fill="rgb(24,12,54)"
 		)
 		points = self.solution.point_set()
-		points.reverse()
+		# sort by depth so oldest segments are drawn on top
+		points.sort(key=lambda p: -p.depth)
 		for point in points:
-			c = 255 - (200 * point.depth) / constants.RECURSION_LIMIT
+			fill = colour_lookup(float(point.depth) / constants.RECURSION_LIMIT)
 			for segment in point.segments(self.solution):
 				end = segment.end(self.solution)
-				draw.line((point.x, point.y, end.x, end.y), fill="rgb({r},{g},{b})".format(r=c, g=c, b=c))
-
+				if end.x >= 0 and end.y >= 0 and end.x <= constants.PLOT_SIZE and end.y <= constants.PLOT_SIZE:
+					draw.line((point.x, point.y, end.x, end.y), fill=fill)
 		im.save("out." + str(seq) + ".png", "PNG")
 
 
@@ -320,17 +300,30 @@ def solve(solution):
 
 
 def new_solution():
+
+	def new_baseterm():
+		return BaseTerm('CNST', innerMultiplier=uniform(-10, 10), outerMultiplier=uniform(-10, 10))
+
 	s = Solution()
-	s.prime()
+	s.length_function = TermSet(init_terms=[new_baseterm()])
+	s.radiance_function = TermSet(init_terms=[new_baseterm()])
+	s.orientation_function = TermSet(init_terms=[new_baseterm()])
+	s.termination_function = TermSet(init_terms=[new_baseterm()])
 	return s
 
 candidates = []
 solutions = []
-fittest = new_solution()
+fittest = Solution()
+fittest.length_function = TermSet(init_terms=[BaseTerm('CNST', innerMultiplier=4, outerMultiplier=4)])
+fittest.radiance_function = TermSet(init_terms=[BaseTerm('CNST', innerMultiplier=3, outerMultiplier=3)])
+fittest.orientation_function = TermSet(init_terms=[BaseTerm('CNST', innerMultiplier=0, outerMultiplier=0)])
+fittest.termination_function = TermSet(init_terms=[BaseTerm('CNST', innerMultiplier=0, outerMultiplier=0)])
+fittest.solve()
+
 solutions.append(fittest)
 last_fit = 0
-this_fit = 0
-pool = Pool(8)
+this_fit = fittest.fitness
+pool = Pool(4)
 
 
 def cross(s1, s2):
@@ -341,15 +334,24 @@ def cross(s1, s2):
 	sn.termination_function = copy.deepcopy(s1.termination_function) if random() > 0.5 else copy.deepcopy(s2.termination_function)
 	return sn
 
+print "Mutation constant:      {}".format(constants.MUTE_VARIABILITY)
+print "Mutation positive bias: {}".format(constants.MUTE_POSITIVE_BIAS)
+print ""
+
 for lap in range(100000):
 	candidates = sorted(solutions, key=lambda s: s.fitness)[:3]
-	solutions = candidates[:]  # keep the best few from previous round
-	solutions = map(lambda s: s.mutate(), solutions)  # stir
-	solutions.extend([cross(s1, s2) for s1 in candidates for s2 in candidates])  # mix up the best few from previous round
-	solutions.append(new_solution())  # add a fresh solution
+
+	# keep and mutate the best few from previous round
+	solutions = [copy.deepcopy(c) for c in candidates]
+	solutions = map(lambda s: s.mutate(), solutions)
+
+	# keep the best few from previous round unmutated
+	solutions.extend([copy.deepcopy(c) for c in candidates for n in range(3)])
+
+	# mix up the functions from the best few from previous round
+	solutions.extend([cross(s1, s2) for s1 in candidates for s2 in candidates])
 
 	solutions = pool.map(solve, solutions)  # compute point-sets and fitnesses
-
 	solutions.append(fittest)  # keep the best from last round, un-mutated
 	fittest = max(solutions, key=lambda s: s.fitness)
 
@@ -360,7 +362,7 @@ for lap in range(100000):
 	if last_fit != this_fit or lap == 0:
 		p = Plot(fittest)
 		p.draw(lap)
-		print "{lap}: {fitness} ({improvement:+.2%})".format(lap=lap, fitness=this_fit, improvement=improvement)
+		print "{lap}: {fitness} ({improvement:+.2%}) from {count} solutions".format(lap=lap, fitness=this_fit, improvement=improvement, count=len(solutions))
 		print "       length:      {length_function}".format(length_function=fittest.length_function.__unicode__())
 		print "       radiance:    {radiance_function}".format(radiance_function=fittest.radiance_function.__unicode__())
 		print "       orientation: {orientation_function}".format(orientation_function=fittest.orientation_function.__unicode__())
